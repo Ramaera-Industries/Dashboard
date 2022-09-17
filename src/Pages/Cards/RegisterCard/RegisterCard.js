b744deac785644418acad8926bb6a290658f7a43
vivek-vimal <vimalvivek50@gmail.com>
2022-09-17 22:27:31 +0530

    /

---
+++ src/Pages/Cards/RegisterCard/RegisterCard.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import CardLayout from '../../../components/CardLayout/CardLayout'
+import LeftSide from './components/LeftSide'
+import RightSide from './components/RightSide'
+import styled from 'styled-components'
+
+const ResponsiveDisplay = styled.div`
+  display: flex;
+  width: 100%;
+  justify-content: space-between;
+  @media only screen and (max-width: 1150px) {
+    flex-direction: column;
+  }
+`
+
+const RegisterCard = () => {
+  return (
+    <CardLayout>
+      <ResponsiveDisplay>
+        <LeftSide />
+        <RightSide />
+      </ResponsiveDisplay>
+    </CardLayout>
+  )
+}
+
+export default RegisterCard
